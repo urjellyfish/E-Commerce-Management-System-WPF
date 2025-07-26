1f2842955125e72cb36a7ffc51461d44d5c1002c
Duic <ducvvm4a@gmail.com>
2025-07-26 21:48:59 +0700

    CRUD product

---
+++ E-CommerceManagementSystem.Presentation/UserWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using E_CommerceManagementSystem.Business.Services;
+using E_CommerceManagementSystem.Repository.Entities;
+
+namespace E_CommerceManagementSystem.Presentation
+{
+    /// <summary>
+    /// Interaction logic for UserWindow.xaml
+    /// </summary>
+    public partial class UserWindow : Window
+    {
+        private ProductService _service = new();
+
+        public UserWindow()
+        {
+            InitializeComponent();
+        }
+
+
+
+
+
+            private void Window_Loaded(object sender, RoutedEventArgs e)
+            {
+                LoadData();
+            }
+
+            private void LoadData()
+            {
+                ProductList.ItemsSource = null;
+                ProductList.ItemsSource = _service.GetAll();
+            }
+
+            private void BtnSearch_Click(object sender, RoutedEventArgs e)
+            {
+                if (string.IsNullOrWhiteSpace(txtKeyword.Text))
+                {
+                    LoadData();
+                }
+
+                var result = _service.Search(txtKeyword.Text.Trim());
+                ProductList.ItemsSource = null;
+
+                if (result.Count > 0)
+                {
+                    ProductList.ItemsSource = result;
+                }
+                else
+                {
+                    MessageBox.Show("Not found");
+                    return;
+                }
+            }
+
+
+
+        }
+    }
+
---
+++ E-CommerceManagementSystem.Repository/Repositories/ProductRepository.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using E_CommerceManagementSystem.Repository.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace E_CommerceManagementSystem.Repository.Repositories
+{
+    public class ProductRepository
+    {
+        private readonly DbContextOptionsBuilder<AppDbContext> _optionsBuilder = new();
+        private AppDbContext _context;
+
+        public List<Product> GetAll()
+        {
+            _context = new();
+            return _context.Products.Include("Category").ToList();
+        }
+
+        public void Add(Product p)
+        {
+            _context = new();
+            _context.Add(p);
+            _context.SaveChanges();
+        }
+
+        public void Update(Product p)
+        {
+            _context = new();
+            _context.Update(p);
+            _context.SaveChanges();
+        }
+
+        public void Delete(Product p)
+        {
+            _context = new();
+            _context.Remove(p);
+            _context.SaveChanges();
+        }
+
+        public int GetMaxId()
+        {
+            _context = new();
+            if (!_context.Products.Any())
+                return 0;
+            return _context.Products.Max(p => p.ProductID);
+        }
+
+        public List<Product> Search(string keyword)
+        {
+            _context = new();
+            keyword = keyword.ToLower().Trim();
+            return _context.Products
+                .Include("Category")
+                .Where(p => p.Category.Name.Contains(keyword))
+                .ToList();
+
+        }
+    }
+}
