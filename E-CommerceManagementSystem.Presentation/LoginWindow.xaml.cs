284cebfd9d34a3f2398cb175601a4e9d0eb56674
nguyenhuy-dev <nguyenhuy.se205@gmail.com>
2025-07-31 00:16:23 +0700

    fix:

--- E-CommerceManagementSystem.Presentation/LoginWindow.xaml.cs
+++ E-CommerceManagementSystem.Presentation/LoginWindow.xaml.cs
@@ -22,18 +22,21 @@ namespace E_CommerceManagementSystem.Presentation
         {
             string patternEmail = @"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$";
 
+            // Email không được để rỗng
             if (EmailTextBox.Text.IsNullOrEmpty())
             {
                 MessageBox.Show("Email is required!", "Input Error", MessageBoxButton.OK, MessageBoxImage.Exclamation);
                 return false;
             }
 
+            // Email phải theo format
             if (!Regex.IsMatch(EmailTextBox.Text.Trim(), patternEmail))
             {
                 MessageBox.Show("Invalid email format!", "Input Error", MessageBoxButton.OK, MessageBoxImage.Exclamation);
                 return false;
             }
 
+            // Password không được để rỗng
             if (PasswordBox.Password.IsNullOrEmpty())
             {
                 MessageBox.Show("Password is required!", "Input Error", MessageBoxButton.OK, MessageBoxImage.Exclamation);
@@ -48,22 +51,26 @@ namespace E_CommerceManagementSystem.Presentation
             if (!Validate())
                 return;
 
+            // Xác thực
             if (!_service.IsAuth(EmailTextBox.Text.Trim(), PasswordBox.Password))
             {
                 MessageBox.Show("Invalid email or password!", "Login Failed", MessageBoxButton.OK, MessageBoxImage.Error);
                 return;
             }
 
+            // Phân quyền
             Customer? customer = _service.GetCustomerByEmail(EmailTextBox.Text.Trim());
-
             if (customer != null)
             {
                 UserWindow userWindow = new UserWindow();
                 userWindow.Customer = customer;
                 userWindow.Show();
             }
-
-
+            else
+            {
+                MainWindow main = new MainWindow();
+                main.Show();
+            }
 
             Hide();
         }
