57dcd46aede32a41825048df46fb4e278fe2fd8e
HuynhNguyen2809 <kdshuynh48964@gmail.com>
2025-07-31 02:20:35 +0700

    add order detail

---
+++ E-CommerceManagementSystem.Presentation/Converter/OrderIdToVisibilityConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace E_CommerceManagementSystem.Presentation.Converter
+{
+    public class OrderIdToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool isNull = value == null;
+            bool invert = parameter?.ToString() == "Invert";
+
+            if (invert)
+            {
+                return isNull ? Visibility.Collapsed : Visibility.Visible;
+            }
+            else
+            {
+                return isNull ? Visibility.Visible : Visibility.Collapsed;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
